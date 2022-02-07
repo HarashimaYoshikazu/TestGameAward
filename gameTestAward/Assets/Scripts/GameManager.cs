5b55d26ccaf076c41e5fd99abadb67cf45f4bcd3
Harashima <harashima318@gmail.com>
2022-02-07 23:58:14 +0900

    [add]GameManagerから視点の切り替えができるように

--- gameTestAward/Assets/Scripts/GameManager.cs
+++ gameTestAward/Assets/Scripts/GameManager.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] CharacterController _human = null;
     [SerializeField] CharacterController _ghost = null;
+    [SerializeField] CinemachineVirtualCamera _vcam = default;
     void Start()
     {
        _human = Instantiate(_human);
        _ghost = Instantiate(_ghost);
        _ghost.enabled = false;
+        _vcam.LookAt = _human.gameObject.transform;
     }
 
     void Update()
@@ -22,15 +25,20 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
+            //ゴーストが動いていたら
             if (_ghost.enabled)
             {
+                //ゴーストの動きを止めて人間を動かす
                 _ghost.enabled = false;
                 _human.enabled = true;
+                //カメラのフォローを人間に変える
+                _vcam.LookAt = _human.gameObject.transform;
             }
             else
             {
                 _ghost.enabled = true;
                 _human.enabled = false;
+                _vcam.LookAt = _ghost.gameObject.transform;
             }
         }
     }
